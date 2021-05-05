49bf0e84fac6abf44db56cdfffee2e7eefc353f5
jebajee <34969219+jebajee@users.noreply.github.com>
2021-05-05 21:43:48 +0800

    Update templates

--- footer.php
+++ footer.php
@@ -11,21 +11,50 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'amenum-core' ) ); ?>">
+	<footer id="site-footer" class="site-footer bg-light">
+  		<div id="footer-main" class="container text-md-left">
+			<div class="row py-3">
+				<div class="col-md-6 mt-md-0 mt-3">
+					<h5 class="text-uppercase"><?php echo get_bloginfo ( 'name' ); ?></h5>
+					<p>Here you can use rows and columns to organize your footer content.</p>
+				</div>
+				<hr class="clearfix w-100 d-md-none pb-3">
+				<div class="col-md-3 mb-md-0 mb-3">
+					<h5 class="text-uppercase"><?php echo wp_get_nav_menu_name('menu-1') ?></h5>
+					<?php
+					wp_nav_menu(
+								array(
+									'theme_location' => 'menu-1',
+									'menu_id'        => 'primary-menu',
+									'menu_class'	 => 'navbar-nav',
+								)
+							);
+							?>
+				</div>
+			<div class="col-md-3 mb-md-0 mb-3">
+				<h5 class="text-uppercase"><?php echo wp_get_nav_menu_name('footer-1') ?></h5>
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'amenum-core' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'amenum-core' ), 'amenum-core', '<a href="http://www.amenum.com">Amenum</a>' );
-				?>
+				wp_nav_menu(
+							array(
+								'theme_location' => 'footer-1',
+								'menu_id'        => 'footer-1',
+								'menu_class'	 => 'navbar-nav',
+							)
+						);
+						?>
+
+			</div>
+			</div>
+			</div>
+		</div> <!-- .footer-main -->
+
+		<div class="site-info footer-copyright text-center bg-dark text-light py-3">
+			<?php printf( esc_html__( 'Copyright &copy; %1$s %2$s', 'amenum-core' ), date("Y"), get_bloginfo ( 'name' ) ); ?>
+			<span class="sep"> &middot; </span>
+			<?php printf( esc_html__( 'Website by %s', 'amenum-core' ), '<a href="http://www.amenum.com">Amenum</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- functions.php
+++ functions.php
@@ -8,22 +8,18 @@
  */
 
 
+if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.0' );
+}
+
+/* Theme CSS */
 add_action( 'wp_enqueue_scripts', 'amenum_enqueue_theme_css' );
 function amenum_enqueue_theme_css() {
     wp_enqueue_style(
         'default',
         get_template_directory_uri() . '/dist/main.css'
     );
-}
-
-add_action( 'after_setup_theme', 'register_navwalker' );
-function register_navwalker(){
-	require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
-}
-
-if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
 }
 
 if ( ! function_exists( 'amenum_core_setup' ) ) :
@@ -60,13 +56,6 @@ if ( ! function_exists( 'amenum_core_setup' ) ) :
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
 		add_theme_support( 'post-thumbnails' );
-
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus(
-			array(
-				'menu-1' => esc_html__( 'Primary', 'amenum-core' ),
-			)
-		);
 
 		/*
 		 * Switch default core markup for search form, comment form, and comments
@@ -165,36 +154,27 @@ function amenum_core_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'amenum_core_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
+/* Implement the Custom Header feature. */
 require get_template_directory() . '/inc/custom-header.php';
 
-/**
- * Custom template tags for this theme.
- */
+/* Register menus */
+require get_template_directory() . '/inc/menus.php';
+
+/*Custom template tags for this theme. */
 require get_template_directory() . '/inc/template-tags.php';
 
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
+/* Functions which enhance the theme by hooking into WordPress. */
 require get_template_directory() . '/inc/template-functions.php';
 
-/**
- * Customizer additions.
- */
+/* Customizer additions. */
 require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Load Jetpack compatibility file.
- */
+/* Load Jetpack compatibility file. */
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-/**
- * Load WooCommerce compatibility file.
- */
+/* Load WooCommerce compatibility file. */
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
